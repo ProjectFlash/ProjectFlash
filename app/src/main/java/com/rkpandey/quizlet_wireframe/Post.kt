94903a2449ae00067d053129c149b32615f32e56
Zhaoshan-Duan <joshua.duan47@gmail.com>
2022-03-18 17:14:40 -0700

    add like count, password helper methods, set name

--- app/src/main/java/com/rkpandey/quizlet_wireframe/Post.kt
+++ app/src/main/java/com/rkpandey/quizlet_wireframe/Post.kt
@@ -41,10 +41,39 @@ class Post : ParseObject() {
         put(KEY_USER, user)
     }
 
+    fun getPassword(): String?{
+        return getString(KEY_PASSWORD)
+    }
+
+    fun setPassword(password: String){
+        put(KEY_PASSWORD, password)
+    }
+
+    fun getLike(): Number?{
+        return getNumber(KEY_LIKE_COUNT)
+    }
+
+    fun setLike(like_count: Number){
+        put(KEY_LIKE_COUNT, like_count)
+    }
+
+    fun getSetName(): String?{
+        return getString(KEY_SET_NAME)
+    }
+
+    fun setSetName(set_name: String){
+        put(KEY_SET_NAME, set_name)
+    }
+    
+    // change to like count
+
     companion object{
         const val KEY_WORD = "word"
         const val KEY_DEFINITION = "definition"
         const val KEY_CATEGORY = "category"
         const val KEY_USER = "user"
+        const val KEY_PASSWORD = "password"
+        const val KEY_LIKE_COUNT = "like_count"
+        const val KEY_SET_NAME = "set_name"
     }
 }
